8b627a55f3a66c51fffb2a39de75ce0c233e4af1
PTE-Repositorio <soportecampus@iudigital.edu.co>
2025-09-25 12:10:14 -0500

    Update contentScript_3.js

--- contentScript_3.js
+++ contentScript_3.js
@@ -1,6 +1,6 @@
-//Eliminar instrucción de la retroalimentación por pregunta
+//Eliminar  la retroalimentación por pregunta
 
- const retroalimentación_por_preg = document.querySelectorAll("#comments_header");
+ const retroalimentación_por_preg = document.querySelectorAll(".answer_comments");
         if (retroalimentación_por_preg.length > 0) {
             retroalimentación_por_preg.forEach(n => n.remove());
             console.log(`✅ Width ajustado input ${retroalimentación_por_preg.length} bloques`);}
