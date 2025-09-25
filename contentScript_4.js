a53c20b4720ce6d5f88ee15e45a25bb600677387
PTE-Repositorio <soportecampus@iudigital.edu.co>
2025-09-25 12:11:33 -0500

    Update contentScript_4.js

--- contentScript_4.js
+++ contentScript_4.js
@@ -1,23 +1,7 @@
-   //Eliminar instrucción de la retroalimentación general de la pregunta
 
-   
-   //instrucción de la retroalimentación  pregunta correcta
-    const nodos_13 = document.querySelectorAll("#correct_label");
+   //retroalimentación  
+    const nodos_13 = document.querySelectorAll(".question_comment");
         if (nodos_13.length > 0) {
             nodos_13.forEach(n => n.remove());
-            console.log(`✅ instrucción de la retroalimentación   pregunta correcta Eliminados ${nodos_13.length} bloques`);}
+            console.log(`✅ la retroalimentación  Generales Eliminados ${nodos_13.length} bloques`);}
 
-            
-   //instrucción de la retroalimentación  pregunta Errada
-
-    const nodos_14 = document.querySelectorAll("#wrong_label");
-        if (nodos_14.length > 0) {
-            nodos_14.forEach(n => n.remove());
-            console.log(`✅  instrucción de la retroalimentación  pregunta Errada Eliminados ${nodos_14.length} bloques`);}
-
-  //instrucción de la retroalimentación  general
-
-    const nodos_15 = document.querySelectorAll("#general_label");
-        if (nodos_15.length > 0) {
-            nodos_15.forEach(n => n.remove());
-            console.log(`✅ instrucción de la retroalimentación  general Eliminados ${nodos_15.length} bloques`);}
