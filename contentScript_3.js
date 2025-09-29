d803bce847002d08724861aced22b66c5b061999
PTE-Repositorio <soportecampus@iudigital.edu.co>
2025-09-29 09:45:26 -0500

    Update contentScript_3.js

--- contentScript_3.js
+++ contentScript_3.js
@@ -1,7 +1,15 @@
 //Eliminar  la retroalimentación por pregunta
 
+try {
+ 
  const retroalimentación_por_preg = document.querySelectorAll(".answer_comments");
         if (retroalimentación_por_preg.length > 0) {
             retroalimentación_por_preg.forEach(n => n.remove());
-            console.log(`✅ Width ajustado input ${retroalimentación_por_preg.length} bloques`);}
+            console.log(`✅ Width ajustado input ${retroalimentación_por_preg.length} bloques`);
 
+    } else {
+        console.warn("⚠️ No se encontraron elementos con la clase .answer_comments");
+    }
+} catch (error) {
+    console.error("❌ Error al intentar clickear los edit:", error);
+}
