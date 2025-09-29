72a0f917b5ca955d1293f1068d23714b7546595e
PTE-Repositorio <soportecampus@iudigital.edu.co>
2025-09-29 09:44:48 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,19 @@
 //Clickeamos el botón editar
 
+
+
+try {
+
   const clic_editar = document.querySelectorAll(".edit_teaser_link");
-        if (clic_editar.length > 0) {
-            clic_editar.forEach(n => n.click());
-            console.log(`✅ Clickeados ${clic_editar.length} botones`);
-        }
+  if (clic_editar.length > 0) {
+    clic_editar.forEach(n => n.click());
+    console.log(`✅ Clickeados ${clic_editar.length} botones`);
+  }
+
+
+  else {
+    console.warn("⚠️ No se encontraron elementos con la clase .edit_teaser_link");
+  }
+} catch (error) {
+  console.error("❌ Error al intentar clickear los edit:", error);
+}
