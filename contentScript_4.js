6be29faa03535d98f82794608a16521bf3af3afb
PTE-Repositorio <soportecampus@iudigital.edu.co>
2025-09-29 09:45:12 -0500

    Update contentScript_4.js

--- contentScript_4.js
+++ contentScript_4.js
@@ -1,7 +1,15 @@
 
    //retroalimentación  
+
+try {
+ 
     const nodos_13 = document.querySelectorAll(".question_comment");
         if (nodos_13.length > 0) {
             nodos_13.forEach(n => n.remove());
             console.log(`✅ la retroalimentación  Generales Eliminados ${nodos_13.length} bloques`);}
-
+  else {
+        console.warn("⚠️ No se encontraron elementos con la clase .question_comment");
+    }
+} catch (error) {
+    console.error("❌ Error al intentar clickear los edit:", error);
+}
